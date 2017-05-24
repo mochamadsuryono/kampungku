4d2f06a4ffb203ef4e4ccdec1854f4fcd7d735eb
iraadellya <iraadellyalestari@gmail.com>
2017-05-24 15:17:25 +0700

    Update moderatesurat.php

--- moderatesurat.php
+++ moderatesurat.php
@@ -169,7 +169,7 @@ font-size: 16px;">&nbsp;
 										}
 										else {
 									?>
-										<p class="w3-center">Tidak ada Request Surat baru</p>
+										<p class="w3-center">Tidak ada request surat baru</p>
 									<?php
 										}
 									?>
